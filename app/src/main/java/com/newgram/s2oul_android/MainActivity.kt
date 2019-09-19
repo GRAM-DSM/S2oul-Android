c45b11751a72fc402d573621d2c8fd0f540522e9
Yeongrae Kim <rayray.yoyo@gmail.com>
2019-09-19 14:12:27 +0900

    Update Initial Setting

--- app/src/main/java/com/newgram/s2oul_android/MainActivity.kt
+++ app/src/main/java/com/newgram/s2oul_android/MainActivity.kt
@@ -2,11 +2,44 @@ package com.newgram.s2oul_android
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var fragmentManager = supportFragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().run {
+            replace(R.id.main_frame, RecommendFragment())
+            commit()
+        }
+        main_menu.setOnNavigationItemSelectedListener(navigationItemSelectedListner)
+    }
+
+    private val navigationItemSelectedListner = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        val transaction = supportFragmentManager.beginTransaction()
+        when (item.itemId) {
+            R.id.navigation_main -> {
+                transaction.replace(R.id.main_frame, RecommendFragment())
+                transaction.commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_info -> {
+                transaction.replace(R.id.main_frame, InfoFragment())
+                transaction.commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_map -> {
+                transaction.replace(R.id.main_frame, MapFragment())
+                transaction.commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+
     }
 }
---
+++ app/src/main/java/com/newgram/s2oul_android/RecommendFragment.kt
@@ -0,0 +1,24 @@
+package com.newgram.s2oul_android
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class RecommendFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recommend, container, false)
+    }
+
+}
