60ba7791b9a968097af88c1ddcb8c331532fe675
Yeongrae Kim <rayray.yoyo@gmail.com>
2019-09-22 17:52:49 +0900

    Update Test Library

--- app/src/main/java/com/newgram/s2oul_android/PlaceDetailActivity.kt
+++ app/src/main/java/com/newgram/s2oul_android/PlaceDetailActivity.kt
@@ -3,10 +3,10 @@ package com.newgram.s2oul_android
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class DetailActivity : AppCompatActivity() {
+class PlaceDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        setContentView(R.layout.activity_place_detail)
     }
 }
---
+++ app/src/main/java/com/newgram/s2oul_android/ShowDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.newgram.s2oul_android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShowDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_detail)
+    }
+}
