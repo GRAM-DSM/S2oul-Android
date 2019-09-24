998b025f644b2ffcaae4cd4f0946b774a1ded901
Yeongrae Kim <rayray.yoyo@gmail.com>
2019-09-24 16:03:55 +0900

    Add detail entities

--- app/src/main/java/com/newgram/s2oul_android/placeDetail/PlaceDetailActivity.kt
+++ app/src/main/java/com/newgram/s2oul_android/placeDetail/PlaceDetailActivity.kt
@@ -3,13 +3,29 @@ package com.newgram.s2oul_android.placeDetail
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.newgram.s2oul_android.R
+import com.newgram.s2oul_android.entities.Showing
+import com.newgram.s2oul_android.showDetail.ShowDetailActivity
+import kotlinx.android.synthetic.main.activity_place_detail.*
+import org.jetbrains.anko.startActivity
 
 class PlaceDetailActivity : AppCompatActivity() {
+
+    var items = ArrayList<Showing>()
+    val adapter = PlaceDetailAdapater(this, items)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_place_detail)
 
-        intent.getStringExtra("name")
+        placeDetail_name_tv.text = intent.getStringExtra("name")
+        placeDetail_number_tv.text
+        placeDetail_address_tv.text
+        placeDetail_sitcount_tv.text
+
+        placeDetail_rv.adapter = adapter
+    }
+
+    fun goShowDetail(name: String) {
+        startActivity<ShowDetailActivity>("name" to name)
     }
 }
--- app/src/main/java/com/newgram/s2oul_android/placeDetail/PlaceDetailAdapater.kt
+++ app/src/main/java/com/newgram/s2oul_android/placeDetail/PlaceDetailAdapater.kt
@@ -4,26 +4,41 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.newgram.s2oul_android.R
+import com.newgram.s2oul_android.entities.Showing
+import kotlinx.android.synthetic.main.item_placedetail_shows.view.*
 
-class PlaceDetailAdapater(val context: Context): RecyclerView.Adapter<PlaceDetailAdapater.PlaceDetailViewHolder>() {
+class PlaceDetailAdapater(val context: Context, val items: List<Showing>): RecyclerView.Adapter<PlaceDetailAdapater.PlaceDetailViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceDetailViewHolder {
-        return PlaceDetailViewHolder(LayoutInflater.from(context).inflate(R.layout.item_photo, parent, false))
+        return PlaceDetailViewHolder(LayoutInflater.from(context).inflate(R.layout.item_placedetail_shows, parent, false))
     }
 
     override fun onBindViewHolder(holder: PlaceDetailViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.run {
+            items[position].let {
+                bind(it)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    inner class PlaceDetailViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        fun bind() {
+    inner class PlaceDetailViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val image = itemView.findViewById<ImageView>(R.id.item_placedetail_iv)
+        fun bind(item: Showing) {
+            with(view) {
+                Glide.with(image)
+                    .load(item.showImage)
+                    .into(image)
 
+                item_placedetail_name_tv.text = item.showName
+            }
         }
     }
 
