d8ed764abd2c4c5fb5b586fdb910d1d2dac6cad8
Yeongrae Kim <rayray.yoyo@gmail.com>
2019-09-24 13:41:02 +0900

    Update placedetail xml

--- app/src/main/java/com/newgram/s2oul_android/placeDetail/PlaceDetailActivity.kt
+++ app/src/main/java/com/newgram/s2oul_android/placeDetail/PlaceDetailActivity.kt
@@ -1,12 +1,15 @@
-package com.newgram.s2oul_android
+package com.newgram.s2oul_android.placeDetail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.newgram.s2oul_android.R
 
 class PlaceDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_place_detail)
+
+        intent.getStringExtra("name")
     }
 }
--- app/src/main/java/com/newgram/s2oul_android/showDetail/ShowDetailActivity.kt
+++ app/src/main/java/com/newgram/s2oul_android/showDetail/ShowDetailActivity.kt
@@ -1,12 +1,16 @@
-package com.newgram.s2oul_android
+package com.newgram.s2oul_android.showDetail
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.newgram.s2oul_android.R
 
 class ShowDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_detail)
+
+        intent.getStringExtra("name")
+
     }
 }
