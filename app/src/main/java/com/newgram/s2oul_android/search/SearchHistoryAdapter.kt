e0a836042cdf12b5d5dc2dca4aec85a10a27d5ff
Yeongrae Kim <rayray.yoyo@gmail.com>
2019-09-28 00:40:58 +0900

    Update searchhistory adapter

--- app/src/main/java/com/newgram/s2oul_android/search/SearchHistoryAdapter.kt
+++ app/src/main/java/com/newgram/s2oul_android/search/SearchHistoryAdapter.kt
@@ -1,14 +1,12 @@
 package com.newgram.s2oul_android.search
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.newgram.s2oul_android.R
-import com.newgram.s2oul_android.dummy
 import java.util.*
 
 class SearchHistoryAdapter(var context: Context, val items: ArrayList<String>): RecyclerView.Adapter<SearchHistoryAdapter.SearchHistoryViewHolder>() {
@@ -25,23 +23,23 @@ class SearchHistoryAdapter(var context: Context, val items: ArrayList<String>): 
         holder.bind(list[position])
     }
 
-    fun filter(text: String) {
-        list.clear()
-        if (text.length == 0) {
-            list = dummy
-        } else {
-            for (i in 0..dummy.size-1) {
-                if (dummy.get(i).toLowerCase().contains(text)) {
-                    list.add(dummy.get(i))
-                    Log.d("Adapter", dummy.get(i))
-                }
-            }
-        }
-        notifyDataSetChanged()
-    }
+//    fun filter(text: String) {
+//        list.clear()
+//        if (text.length == 0) {
+//            list = dummy
+//        } else {
+//            for (i in 0..dummy.size-1) {
+//                if (dummy.get(i).toLowerCase().contains(text)) {
+//                    list.add(dummy.get(i))
+//                    Log.d("Adapter", dummy.get(i))
+//                }
+//            }
+//        }
+//        notifyDataSetChanged()
+//    }
 
-    inner class SearchHistoryViewHolder(v: View): RecyclerView.ViewHolder(v) {
-        val searchHistoryTv = v.findViewById<TextView>(R.id.searchHistory_history_tv)
+    inner class SearchHistoryViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+        val searchHistoryTv = view.findViewById<TextView>(R.id.searchHistory_history_tv)
 
         fun bind(name: String) {
             searchHistoryTv.text = name
