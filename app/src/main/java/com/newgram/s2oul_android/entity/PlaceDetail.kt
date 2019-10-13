aed53547e4d5422abac99a0f07f703f6a9dd528d
Yeongrae Kim <rayray.yoyo@gmail.com>
2019-10-13 20:31:17 +0900

    Update naming

--- app/src/main/java/com/newgram/s2oul_android/entity/PlaceDetail.kt
+++
@@ -1,9 +0,0 @@
-package com.newgram.s2oul_android.entity
-
-data class PlaceDetail(
-    var name: String,
-    var number: String,
-    var address: String,
-    var sitCount: String,
-    var shows: List<Showing>
-) {}
--- app/src/main/java/com/newgram/s2oul_android/entity/ShowDetail.kt
+++
@@ -1,9 +0,0 @@
-package com.newgram.s2oul_android.entity
-
-data class ShowDetail(
-    var name: String,
-    var age: String,
-    var price: String,
-    var place: PlaceDetail,
-    var story: List<String>
-) {}
